0b04f6bbae8f3bed35e8b37fb1fd77fba94edf1e
odipojames <odipojames12@gmai.com>
2018-07-13 08:59:31 +0300

    remove list on second colum

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
+(document).ready(function() {
+  $("form1#formd").submit(function(event) {
+    event.preventDefault();
+    var userInput = $("input#digit").val();
+    console.log(userInput);
+    $("ul.result").text(userInput);
+  });
 
+});
