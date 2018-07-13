62ce32e27ec524e389e8e1571f5805f8a6309b61
odipojames <odipojames12@gmai.com>
2018-07-13 09:30:06 +0300

    resize colum 2

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,23 @@
     var userInput = $("input#digit").val();
     console.log(userInput);
     $("ul.result").text(userInput);
-  });
-
+  
+function pingPong(){
+  for (var i=1; x <= userInput; i++) {
+    var skip = 0;
+    if (i % 3 == 0) {
+        document.write('ping');
+        skip = 1;
+    }
+    if (i % 5 == 0) {
+        document.write('pong');
+        skip = 1;
+    }
+    if (!skip) {
+        document.write(i);
+    }
+    document.write('<br>');
+};
+};
+});
 });
