8d9302caf5967dbda7b7e6d866499ddaa2d6fc25
odipojames <odipojames12@gmai.com>
2018-07-13 12:23:09 +0300

    create pingping function

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,28 @@
-(document).ready(function() {
-  $("form1#formd").submit(function(event) {
+
+
+$(document).ready(function(){
+  $("form#formd").submit(function(event) {
     event.preventDefault();
-    var userInput = $("input#digit").val();
-    console.log(userInput);
-    $("ul.result").text(userInput);
-  
-function pingPong(){
-  for (var i=1; x <= userInput; i++) {
-    var skip = 0;
-    if (i % 3 == 0) {
-        document.write('ping');
-        skip = 1;
-    }
-    if (i % 5 == 0) {
-        document.write('pong');
-        skip = 1;
-    }
-    if (!skip) {
-        document.write(i);
-    }
-    document.write('<br>');
-};
-};
+    var n = parseInt($('input#digit').val());
+    var outPut= pingPong(n);
+    console.log(n);
+      $("#result").text(outPut);
+  });
 });
-});
+
+var pingPong = function(n) {
+
+    var pi = 'Ping';
+    var po = 'Pong';
+    for(i=1; i <= n; i++)
+        if(i % 15===0){
+          console.log(pi + po);
+        }else if(i % 5){
+          console.log(po);
+        }else if(i % 3){
+          console.log(pi);
+        }  else{
+              console.log(i);
+        }
+    return;
+}
