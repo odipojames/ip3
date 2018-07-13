8f5b694a330b2176ddf70afd84ff4ebb2a8d490c
odipojames <odipojames12@gmai.com>
2018-07-13 17:35:00 +0300

    add .empty method

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,29 @@
-
-
-$(document).ready(function(){
+$(document).ready(function() {
   $("form#formd").submit(function(event) {
     event.preventDefault();
+    $('#result').empty();
     var n = parseInt($('input#digit').val());
-    var outPut= pingPong(n);
+    var outPut = pingPong(n);
+
 
   });
 });
 
 var pingPong = function(n) {
 
-    var pi = 'Ping';
-    var po = 'Pong';
-    for(i=1; i <= n; i++){
-        if(i % 15===0){
-        var li="<li>"+ pi+po+"</li>"
-        }else if(i % 5===0){
-          var li="<li>"+po+"</li>"
-        }else if(i % 3===0){
-        var li="<li>"+ pi+"</li>"
-        }  else{
-            var li="<li>"+i+"</li>"
-        }
+  var pi = 'Ping';
+  var po = 'Pong';
+  for (i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      var li = "<li>" + pi + po + "</li>"
+    } else if (i % 5 === 0) {
+      var li = "<li>" + po + "</li>"
+    } else if (i % 3 === 0) {
+      var li = "<li>" + pi + "</li>"
+    } else {
+      var li = "<li>" + i + "</li>"
+    }
 
-  $("#result").append(li);
-}
+    $("#result").append(li);
+  }
 }
